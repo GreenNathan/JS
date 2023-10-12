96edb4459f39fae27ec416875e60740045ca74fa
Nathan Green <greennathan6292@yahoo.com>
2023-10-12 13:48:24 -0700

    jsx

---
+++ CoreAssignments/firstJSX/vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <h1>Hello Dojo</h1>
+    <h2>Things i need to do:</h2>
+    <ul>
+      <li>Learn react</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+
+    </>
+  )
+}
+
+export default App
